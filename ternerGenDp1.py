afcb34494fd7501831fed236ce86c81220804381
Yudhistira Erlandinata <yerlandinata@gmail.com>
2017-09-26 20:47:03 +0700

    update modulo

--- ternerGenDp1.py
+++ ternerGenDp1.py
@@ -1,10 +1,10 @@
-dp = [0 for i in range(1001)]
+dp = [0 for i in range(100005)]
 
 dp[0] = 0
 dp[1] = 0
 dp[2] = 2
 
-MOD = 2 ** 16
+MOD = (10 ** 9) + 7
 
 def modMul(a, b):
     return a * b
@@ -18,8 +18,10 @@ def modSub(a, b):
     return a - b
     # return ((a % MOD) - (b % MOD)) % MOD
 
-for i in range(3, 1001):
+for i in range(3, 100005):
     dp[i] = (modSub( modMul(5, dp[i - 1]),  modAdd( modMul(5, dp[i - 2]), modMul(3, dp[i - 3])     )   )) % MOD
 
-n = int(input())
-print(dp[n])
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(dp[n])
